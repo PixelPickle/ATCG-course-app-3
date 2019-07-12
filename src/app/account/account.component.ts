e2fa4b72d9308c7af1c579e90ea7dd56bdf905f3
PixelPickle <bates.kaylen@gmail.com>
2019-07-12 16:25:37 -0500

    Up To: 114

--- src/app/account/account.component.ts
+++ src/app/account/account.component.ts
@@ -1,5 +1,7 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 
+import {AccountsService} from '../services/accounts.service';
+
 @Component({
   selector: 'app-account',
   templateUrl: './account.component.html',
@@ -8,11 +10,14 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
 export class AccountComponent {
   @Input() account: {name: string, status: string};
   @Input() id: number;
-  @Output() statusChanged = new EventEmitter<{id: number, newStatus: string}>();
 
+  constructor( private accountsService: AccountsService) {
+
+  }
 
   onSetTo(status: string) {
-    this.statusChanged.emit({id: this.id, newStatus: status});
-    console.log('A server status changed, new status: ' + status);
+    this.accountsService.updateStatus(this.id, status);
+    // this.loggingService.logStatusChange(status);
+    this.accountsService.statusUpdated.emit(status);
   }
 }
